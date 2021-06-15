01c6750512c07002bf4e21c485589705e16af047
Vadim <gabderahmanov99@gmail.com>
2021-06-15 17:00:33 +0300

    add webview

--- app/src/main/java/com/vad/volsuproject/MainActivity.java
+++ app/src/main/java/com/vad/volsuproject/MainActivity.java
@@ -1,19 +1,28 @@
 package com.vad.volsuproject;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.vad.volsuproject.pushnotification.JobSchedulerHelper;
 import com.vad.volsuproject.socketresponse.Client;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+    private String URL = "https://lk.volsu.ru/student/index";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //JobSchedulerHelper.jobScheduler(this);
+
+        mWebView = (WebView) findViewById(R.id.webViewVolsu);
+        mWebView.loadUrl(URL);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
 
         new Thread(new Runnable() {
             @Override
